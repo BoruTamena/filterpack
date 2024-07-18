3d333ce6151ce16be0374ab8023f59f9fd6c8938
BoruTamena <borutamena46@gmail.com>
2024-07-18 16:37:15 +0300

    sort feature implemented

--- sorting/sort.go
+++ sorting/sort.go
@@ -1,1 +1,41 @@
 package sorting
+
+import (
+	"errors"
+
+	"github.com/BoruTamena/query"
+	"github.com/gin-gonic/gin"
+)
+
+var (
+	c = &gin.Context{}
+)
+
+type SortResponseQuery struct {
+	Asc string
+	Dsc string
+}
+
+func Sort() (error, SortResponseQuery) {
+
+	var st query.SortParam
+
+	err, sp := st.BindSort(c)
+
+	if err != nil {
+
+		return err, SortResponseQuery{}
+	}
+
+	if sp.Key != "" {
+		return errors.New("please specify a filed name "), SortResponseQuery{}
+	}
+
+	res := SortResponseQuery{
+		Asc: sp.AscSort(),
+		Dsc: sp.DscSort(),
+	}
+
+	return nil, res
+
+}
